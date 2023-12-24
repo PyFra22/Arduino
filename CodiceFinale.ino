cc5004a17310fdcb7af5d7ec1677c6fc6fa5d6da
PyFra22 <116340901+PyFra22@users.noreply.github.com>
2023-12-24 22:50:22 +0100

    Versione 1.1

--- CodiceFinale.ino
+++ CodiceFinale.ino
@@ -10,6 +10,8 @@ LiquidCrystal_I2C lcdDisplay(0x27, 20, 4);
 int sterzoJoystickPinY = A0;
 int speedJoystickPin = A1;
 int pinSterzoServo = 9;
+int pinPowerLCD = 8;
+
 
 int valueSterzi, valueSpeed;
 
@@ -27,25 +29,46 @@ void setup() {
   Wire.begin();
   lcdDisplay.begin(20, 4);
   lcdDisplay.backlight();
+  lcdDisplay.clear();
+  pinMode(pinPowerLCD, OUTPUT);
 
 }
 
 void loop() {
 
+
+  // Get the data from analogic pin
   valueSterzi = analogRead(sterzoJoystickPinY);
   valueSpeed = analogRead(speedJoystickPin);
+
+  // Map the values 
   valueSterzi = map(valueSterzi, 0, 1023, 0, 180);
-  valueSpeed = map(valueSpeed, 512, 1023, 0, 100);
+  valueSpeed = map(valueSpeed, 512, 1023, 50, 100);
   
-  lcdDisplay.clear();
+  // Set and print what we want on the lcd 20x4
   lcdDisplay.setCursor(0, 0);
-  lcdDisplay.print("Direzione Sterzo");
+  lcdDisplay.print("Direzione Sterzo: ");
 
-  if(va)
+  if(valueSterzi > 95 && valueSterzi < 105){
+    lcdDisplay.setCursor(18, 0);
+    lcdDisplay.print("Cx");
+  }
+  else if(valueSterzi < 95){
+    lcdDisplay.setCursor(18, 0);
+    lcdDisplay.print("Sx");
+  }
+  else if(valueSterzi > 105){
+    lcdDisplay.setCursor(18, 0);
+    lcdDisplay.print("Dx");
+  }
   
 
-  sterzoServo.write(valueSterzi);
-  
- 
+  lcdDisplay.setCursor(0, 1);
+  lcdDisplay.print("Ctrl Velocita: ");
+  lcdDisplay.setCursor(16, 1);
+  lcdDisplay.print(valueSpeed);
+  //lcdDisplay.setCursor(19, 1);
+  //lcdDisplay.print("%");
+
 
 }
